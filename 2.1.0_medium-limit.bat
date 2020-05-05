9a1c8192864b5f020cc0d02c354e0e7fe7a0ef3e
kidokun153 <kidokun153@gmail.com>
2020-05-06 00:17:30 +0900

    first commit

---
+++ 2.1.0_medium-limit.bat
@@ -0,0 +1,4 @@
+rem ���O�ύX
+ren "$FilePath$" "$FileName$($AddKey$).ts"
+
+"C:\PX-Q3PE4\Amatsukaze\exe_files\AmatsukazeAddTask.exe" -r "C:\PX-Q3PE4\Amatsukaze" -f "$FolderPath$\$FileName$($AddKey$).ts" -ip "localhost" -p 32768 -o "D:\video\tmp" -s "2.1.0_medium-limit" --priority 3 --no-move
---
+++ 2.2.0_medium-save.bat
@@ -0,0 +1,4 @@
+rem ���O�ύX
+ren "$FilePath$" "$FileName$($AddKey$).ts"
+
+"C:\PX-Q3PE4\Amatsukaze\exe_files\AmatsukazeAddTask.exe" -r "C:\PX-Q3PE4\Amatsukaze" -f "$FolderPath$\$FileName$($AddKey$).ts" -ip "localhost" -p 32768 -o "D:\video\tmp" -s "2.2.0_medium-save" --priority 3 --no-move
